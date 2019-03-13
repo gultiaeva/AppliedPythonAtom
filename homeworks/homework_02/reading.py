c0af7674a6ba110da34dd95d07e53c21c67c5953
gultiaeva <angultiaev@gmail.com>
2019-03-13 15:50:23 +0300

    finally final

--- homeworks/homework_02/reading.py
+++ homeworks/homework_02/reading.py
@@ -60,29 +60,3 @@ def read_json(filename, enc):
             data[i].append(val)
 
     return data
-
-'''
-def is_json(filename, enc):
-    try:
-        with open(filename, encoding=enc) as f_obj:
-            json.load(f_obj)
-            return True
-    except json.JSONDecodeError:
-        return False
-
-
-def is_tsv(filename, enc):
-    try:
-        with open(filename, encoding=enc) as f_obj:
-            csv.reader(f_obj, delimiter="\t")
-            return True
-    except csv.Error:
-        return False
-
-
-def get_format(filename, enc):
-    if is_json(filename, enc):
-        return 'json'
-    elif is_tsv(filename, enc):
-        return "tsv"
-'''
