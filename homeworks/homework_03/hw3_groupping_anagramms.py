d43a656a4cb5cc603019850ae571a4f298644e2e
gultiaeva <angultiaev@gmail.com>
2019-03-16 20:05:08 +0300

    done hw3_groupping_anagramms.py

--- homeworks/homework_03/hw3_groupping_anagramms.py
+++ homeworks/homework_03/hw3_groupping_anagramms.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+import itertools
 
 
 def groupping_anagramms(words):
@@ -7,20 +8,34 @@ def groupping_anagramms(words):
     Функция, которая группирует анаграммы.
     Возвращаем массив, где элементом является массив с анаграмами.
     Пример:  '''Аз есмь строка живу я мерой остр
-                За семь морей ростка я вижу рост
-                Я в мире сирота
-                Я в Риме Ариост'''.split()
+               За семь морей ростка я вижу рост
+               Я в мире сирота
+               Я в Риме Ариост'''.split()
                 ->
                 [
-                 ['Аз'], ['есмь', 'семь'],
-                 ['строка', 'ростка'], ['живу', 'вижу'],
-                 ['я', 'я'], ['мерой', 'морей'],
-                 ['остр)'], ['За'], ['рост'], ['Я', 'Я'],
-                 ['в', 'в'], ['мире'], ['сирота'],
-                 ['Риме'], ['Ариост']
+                    ['Аз', 'За'], ['Ариост', 'сирота'],
+                    ['Риме', 'мире'], ['Я', 'Я', 'я', 'я'],
+                    ['в', 'в'], ['вижу', 'живу'],
+                    ['есмь', 'семь'], ['мерой', 'морей'],
+                    ['остр', 'рост'], ['ростка', 'строка']
                 ]
     :param words: list of words (words in str format)
     :return: list of lists of words
     """
-    # TODO: реализовать функцию
-    raise NotImplementedError
+    res = []
+    for raw_word in words:
+        tmp = []
+        word = raw_word.lower()
+        if len(word) == 1:
+            tmp = [word1 for word1 in words if word1.lower() == word]
+            if all(set(tmp) != set(perm) for perm in res):
+                res.append(tmp)
+            continue
+        unique_permutations = set(''.join(comb).lower()
+                                  for comb in itertools.permutations(word))
+        for word1 in words:
+            if word1.lower() in unique_permutations:
+                tmp.append(word1)
+        if all(set(tmp) != set(perm) for perm in res):
+            res.append(tmp)
+    return res
