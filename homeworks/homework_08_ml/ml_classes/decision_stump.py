266d78e68e4a6acdba6806271e78dc72e4f7de0e
gultiaeva <angultiaev@gmail.com>
2019-04-29 13:53:00 +0300

    kaggle done

--- homeworks/homework_08_ml/ml_classes/decision_stump.py
+++ homeworks/homework_08_ml/ml_classes/decision_stump.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import numpy as np
+
 
 class DecisionStumpRegressor:
     '''
@@ -13,7 +15,7 @@ class DecisionStumpRegressor:
         Мы должны создать поля, чтобы сохранять наш порог th и ответы для
         x <= th и x > th
         '''
-        raise NotImplementedError
+        pass
 
     def fit(self, X, y):
         '''
@@ -22,7 +24,23 @@ class DecisionStumpRegressor:
         :param y: целевая переменная (1, num_objects)
         :return: None
         '''
-        raise NotImplementedError
+        assert X.shape[0] == y.shape[0], "shapes don't match"
+        X, y = zip(*sorted(zip(X, y), key=lambda x: x[0]))
+        X, y = np.array(X), np.array(y)
+        hall = np.average(np.square(X - X.mean()))
+        n = len(y)
+        qs = []
+        for i in range(1, n):
+            th = ((X[i-1] + X[i]) / 2)[0]
+            left, right = X[X < th], X[X > th]
+            hleft = np.average(np.square(left - left.mean()))
+            hright = np.average(np.square(right - right.mean()))
+            q = hall - (len(left) * hleft + len(right) * hright) / n
+            qs.append((th, q))
+
+        self.th = max(qs, key=lambda x: x[1])[0]
+        self.left = y[(X <= self.th).flatten()].mean()
+        self.right = y[(X > self.th).flatten()].mean()
 
     def predict(self, X):
         '''
@@ -30,4 +48,4 @@ class DecisionStumpRegressor:
         :param X: массив размера (1, num_objects)
         :return: массив, размера (1, num_objects)
         '''
-        raise NotImplementedError
+        return np.array([self.left, self.right])[(X >= self.th).astype('int64')]
